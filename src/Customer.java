876f69b2be09f84841ebb53670a5d2c9e3e18388
Hanlin Zou <haz272@ucsd.edu>
2021-12-09 21:07:40 +0800

    新增了用户开户功能，我好烦，队友在吃饭在回家路上，不想写了躺平了

--- src/Customer.java
+++ src/Customer.java
@@ -1,2 +1,39 @@
+import java.util.ArrayList;
+
 public class Customer {
+    private String name;
+    private ArrayList<Loan> loanArrayList;
+    private Saving savingAcc;
+    private Checking checkingAcc;
+
+    Customer(){}
+
+    Customer(String name){
+        setName(name);
+        loanArrayList = new ArrayList<>();
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void openSavingAccount(){
+        savingAcc = new Saving();
+    }
+
+    public void closeSavingAccount(){
+        savingAcc = null;
+    }
+
+    public void openCheckingAccount(){
+        checkingAcc = new Checking();
+    }
+
+    public void closeCheckingAccount(){
+        checkingAcc = null;
+    }
 }
