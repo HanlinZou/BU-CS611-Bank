d3db69ca6fa5060f247bb02be1a3d67b93aadcef
Hanlin Zou <haz272@ucsd.edu>
2021-12-10 18:53:10 +0800

    为了省心，将不在用户补仓时重新计算成本单价，而是他妈的直接在持有列表里加新的股票，新增了几个计算（调整）收益的方程，他妈的吃了饭再写

--- src/StockAccount.java
+++ src/StockAccount.java
@@ -3,19 +3,23 @@ import java.util.ArrayList;
 public class StockAccount {
     public double balance;
     public ArrayList<BoughtStock> stocks;
-    public double estimatedEarn;
-    public double accumulatedEarn;
+    public double estimatedProfit;
+    public double accumulatedProfit;
 
     StockAccount(){}
 
     StockAccount(double balance){
         this.balance = balance;
         stocks = new ArrayList<>();
-        estimatedEarn = 0;
-        accumulatedEarn = 0;
+        estimatedProfit = 0;
+        accumulatedProfit = 0;
     }
 
     public void changeBalance(double amt){
         balance = Double.parseDouble(String.format("%.2f", balance + amt));
     }
+
+    public void calculateEstimatedProfit(){}
+    public void sellStock(double latestPrice){}
+    public void adjustAccumulatedProfit(){}
 }
