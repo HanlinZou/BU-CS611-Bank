5416a786d2cb2680ad34a1c2e564acdfb0deba35
Hanlin Zou <haz272@ucsd.edu>
2021-12-10 20:57:04 +0800

    新增了股票账户的计算预期收益，卖股票的功能（等妹子写），调整累计收益的功能

--- src/StockAccount.java
+++ src/StockAccount.java
@@ -19,7 +19,32 @@ public class StockAccount {
         balance = Double.parseDouble(String.format("%.2f", balance + amt));
     }
 
-    public void calculateEstimatedProfit(){}
-    public void sellStock(double latestPrice){}
-    public void adjustAccumulatedProfit(){}
+    public void calculateEstimatedProfit(){
+        estimatedProfit = 0;
+        double cost, latestPrice;
+        for (BoughtStock stock : stocks) {
+            cost = stock.purchasePrice;
+            // TODO: read the current price of this stock!
+            latestPrice = 1;
+            estimatedProfit += (latestPrice - cost) * stock.share;
+        }
+        estimatedProfit = Double.parseDouble(String.format("%.2f", estimatedProfit));
+    }
+
+    /**
+     * Sell a customer's stock
+     * @param latestPrice the latest price the stock
+     * @param share how much share the customer wants to sale
+     */
+    public void sellStock(double latestPrice, double share){
+        /*TODO: 1. find the stock customer wants to sell in his stock list
+                2. calculate the total sell price, add it to balance
+                3. adjust remaining share, if 0, remove this stock from list
+        */
+
+    }
+    public void adjustAccumulatedProfit(double amt){
+        accumulatedProfit = Double.parseDouble(String.format("%.2f", accumulatedProfit + amt));
+        calculateEstimatedProfit();
+    }
 }
